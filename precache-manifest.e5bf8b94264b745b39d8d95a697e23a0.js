0b11f6dd02637873c716a2cd71ce8806df6076be
Gursandeep Kainth <Gursandeep@zenithpayments.com.au>
2020-03-03 00:24:05 +1100

    Updates

---
+++ precache-manifest.e5bf8b94264b745b39d8d95a697e23a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66e97e3d38e7f9d99d8be96db081e038",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "32b0fed47813562abc00",
+    "url": "/monsters-rolodex/static/css/main.7c6541a4.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32b0fed47813562abc00",
+    "url": "/monsters-rolodex/static/js/main.65f5493e.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
